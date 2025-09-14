bf48ca2e50c40170fa30624aab0ca844f9facb35
M0hammedMahm0ud <moseleeeem12@gmail.com>
2025-09-14 18:45:52 +0300

    add useLoaderData instead of useEffect

--- index.jsx
+++ index.jsx
@@ -1,14 +1,13 @@
 import ReactDOM from "react-dom/client";
 import {
+  RouterProvider,
+  createBrowserRouter,
   createRoutesFromElements,
   Route,
-  createBrowserRouter,
-  RouterProvider,
 } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
-import { NotFound } from "./pages/NotFound";
-import Vans from "./pages/Vans/Vans";
+import Vans, { loader as vansLoader } from "./pages/Vans/Vans";
 import VanDetail from "./pages/Vans/VanDetail";
 import Dashboard from "./pages/Host/Dashboard";
 import Income from "./pages/Host/Income";
@@ -18,35 +17,45 @@ import HostVanDetail from "./pages/Host/HostVanDetail";
 import HostVanInfo from "./pages/Host/HostVanInfo";
 import HostVanPricing from "./pages/Host/HostVanPricing";
 import HostVanPhotos from "./pages/Host/HostVanPhotos";
+import { NotFound } from "./pages/NotFound";
+import Login from "./pages/Login";
 import Layout from "./components/Layout";
 import HostLayout from "./components/HostLayout";
+import Error from "./components/Error";
 
 import "./server";
 
-function App() {
-  const router = createBrowserRouter(
-    createRoutesFromElements(
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route path="about" element={<About />} />
-        <Route path="vans" element={<Vans />} />
-        <Route path="vans/:id" element={<VanDetail />} />
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route index element={<Home />} />
+      <Route path="about" element={<About />} />
+      <Route path="login" element={<Login />} />
+      <Route
+        path="vans"
+        element={<Vans />}
+        errorElement={<Error />}
+        loader={vansLoader}
+      />
+      <Route path="vans/:id" element={<VanDetail />} />
 
-        <Route path="host" element={<HostLayout />}>
-          <Route index element={<Dashboard />} />
-          <Route path="income" element={<Income />} />
-          <Route path="reviews" element={<Reviews />} />
-          <Route path="vans" element={<HostVans />} />
-          <Route path="vans/:id" element={<HostVanDetail />}>
-            <Route index element={<HostVanInfo />} />
-            <Route path="pricing" element={<HostVanPricing />} />
-            <Route path="photos" element={<HostVanPhotos />} />
-          </Route>
+      <Route path="host" element={<HostLayout />}>
+        <Route index element={<Dashboard />} />
+        <Route path="income" element={<Income />} />
+        <Route path="reviews" element={<Reviews />} />
+        <Route path="vans" element={<HostVans />} />
+        <Route path="vans/:id" element={<HostVanDetail />}>
+          <Route index element={<HostVanInfo />} />
+          <Route path="pricing" element={<HostVanPricing />} />
+          <Route path="photos" element={<HostVanPhotos />} />
         </Route>
-        <Route path="*" element={<NotFound />} />
       </Route>
-    )
-  );
+      <Route path="*" element={<NotFound />} />
+    </Route>
+  )
+);
+
+function App() {
   return <RouterProvider router={router} />;
 }
 
--- pages/Vans/Vans.jsx
+++ pages/Vans/Vans.jsx
@@ -1,34 +1,20 @@
 import React from "react";
-import { Link, useSearchParams } from "react-router-dom";
+import { Link, useLoaderData, useSearchParams } from "react-router-dom";
 import { getVans } from "../../api";
 
+export function loader() {
+  return getVans();
+}
 export default function Vans() {
   const [searchParams, setSearchParams] = useSearchParams();
-  const [vans, setVans] = React.useState([]);
-  const [loading, setLoading] = React.useState(false);
   const [error, setError] = React.useState(null);
 
   const typeFilter = searchParams.get("type");
-
-  React.useEffect(() => {
-    async function loadVans() {
-      setLoading(true);
-      try {
-        const data = await getVans();
-        setVans(data);
-      } catch (err) {
-        setError(err);
-      } finally {
-        setLoading(false);
-      }
-    }
-
-    loadVans();
-  }, []);
+  const data = useLoaderData();
 
   const displayedVans = typeFilter
-    ? vans.filter((van) => van.type === typeFilter)
-    : vans;
+    ? data.filter((van) => van.type === typeFilter)
+    : data;
 
   const vanElements = displayedVans.map((van) => (
     <div key={van.id} className="van-tile">
@@ -61,10 +47,6 @@ export default function Vans() {
       }
       return prevParams;
     });
-  }
-
-  if (loading) {
-    return <h1 aria-live="polite">Loading...</h1>;
   }
 
   if (error) {

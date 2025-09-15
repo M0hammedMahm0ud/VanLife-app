95a12437915405f8356ccbefe76d524ce3e1dd6e
M0hammedMahm0ud <moseleeeem12@gmail.com>
2025-09-16 02:04:55 +0300

    using action and Form from react router instead of native form

--- pages/Login.jsx
+++ pages/Login.jsx
@@ -1,33 +1,37 @@
-import { useState } from "react";
-import { useLoaderData } from "react-router";
+import React from "react";
+import { useLoaderData, useNavigate, redirect, Form } from "react-router-dom";
 import { loginUser } from "../api";
+
 export function loader({ request }) {
   return new URL(request.url).searchParams.get("message");
 }
-export default function Login() {
-  const [loginFormData, setLoginFormData] = useState({
-    email: "",
-    password: "",
-  });
-  const [status, setStatus] = useState("idle");
-  const [error, setError] = useState(null);
 
+export async function action({ request }) {
+  const formData = await request.formData();
+  const email = formData.get("email");
+  const password = formData.get("password");
+  const data = await loginUser({ email, password });
+  console.log(data);
+
+  return null;
+}
+
+export default function Login() {
+  const [status, setStatus] = React.useState("idle");
+  const [error, setError] = React.useState(null);
   const message = useLoaderData();
+  const navigate = useNavigate();
 
   function handleSubmit(e) {
     e.preventDefault();
     setStatus("submitting");
+    setError(null);
     loginUser(loginFormData)
-      .then((data) => console.log(data))
+      .then((data) => {
+        navigate("/host", { replace: true });
+      })
       .catch((err) => setError(err))
       .finally(() => setStatus("idle"));
-  }
-  function handleChange(e) {
-    const { name, value } = e.target;
-    setLoginFormData((prev) => ({
-      ...prev,
-      [name]: value,
-    }));
   }
 
   return (
@@ -35,25 +39,14 @@ export default function Login() {
       <h1>Sign in to your account</h1>
       {message && <h3 className="red">{message}</h3>}
       {error && <h3 className="red">{error.message}</h3>}
-      <form onSubmit={handleSubmit} className="login-form">
-        <input
-          name="email"
-          onChange={handleChange}
-          type="email"
-          placeholder="Email address"
-          value={loginFormData.email}
-        />
-        <input
-          name="password"
-          onChange={handleChange}
-          type="password"
-          placeholder="Password"
-          value={loginFormData.password}
-        />
+
+      <Form method="post" className="login-form">
+        <input name="email" type="email" placeholder="Email address" />
+        <input name="password" type="password" placeholder="Password" />
         <button disabled={status === "submitting"}>
-          {status === "submitting" ? "logging in ..." : "log in"}
+          {status === "submitting" ? "Logging in..." : "Log in"}
         </button>
-      </form>
+      </Form>
     </div>
   );
 }

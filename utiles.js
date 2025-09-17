2b34a643179ad0a3c58309a26921adaf291548ad
M0hammedMahm0ud <moseleeeem12@gmail.com>
2025-09-17 03:53:35 +0300

    send user to /host route after log in

--- utiles.js
+++ utiles.js
@@ -1,7 +1,7 @@
 import { redirect } from "react-router-dom";
 
 export async function AuthRequired() {
-  const isLoggedIn = false;
+  const isLoggedIn = localStorage.getItem("loggedin");
   if (!isLoggedIn) {
     throw redirect("/login?message=you must login first");
   }
